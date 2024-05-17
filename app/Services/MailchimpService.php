a45729b9c6e4a93b8d95d4cdfbff0814ebd9d5d4
Haseeb Khalid <david.brandboosters@gmail.com>
2024-05-17 17:26:47 +0500

    loader changes

--- app/Services/MailchimpService.php
+++ app/Services/MailchimpService.php
@@ -17,10 +17,10 @@ class MailchimpService
     {
         $this->client = new Client([
             'base_uri' => 'https://us18.api.mailchimp.com/3.0/',
-            'auth' => ['apikey', '439689f6e36138090811c1c466db0f5e-us18']
+            'auth' => ['apikey', 'xxxxxxxxxx']
         ]);
 
-        $this->listId = '133ac0d1ba';
+        $this->listId = 'xxxxxx';
     }
 
     public function subscribe($email)
